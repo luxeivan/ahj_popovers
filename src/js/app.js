f3eb01a1bc8a6a00b0cdbbb3716291d6423c8f4d
luxeivan <31414039+luxeivan@users.noreply.github.com>
2022-06-08 10:10:35 +0300

    Initial commit

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+import CartValidator from './CardValidator';
+
+const placeForValidator = document.querySelector('.container');
+const cartValidator = new CartValidator(placeForValidator);
+console.log(cartValidator);
